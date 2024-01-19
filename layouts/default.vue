00a4e4b68517a027fb5929127bad1b09a8e3842a
Iskender-crypto <iskenderazamov@gmail.com>
2024-01-19 22:48:33 +0600

    commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@ const themeStore = useThemeStore()
 const products = ref([
   {
     title: 'У нас дешевле',
-    backgroundImage: 'https://images.unsplash.com/photo-1494438639946-1ebd1d20bf85?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2067&q=80',
+    backgroundImage: 'https://images.unsplash.com/photo-1526779259212-939e64788e3c?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     description: 't is a long established fact that a reader will be distracted by the readable content'
   },
   {
